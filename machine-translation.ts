b5ab88330ac267dcbc4af0df5f24d54d024ac98a
Yusuke Kamiyamane <y@y14e.com>
2026-06-27 14:06:47 +0900

    Update machine-translation.ts

--- machine-translation.ts
+++ machine-translation.ts
@@ -1,7 +1,7 @@
 /**
  * machine-translation.ts
  *
- * @version 1.0.5
+ * @version 1.0.6
  * @author Yusuke Kamiyamane
  * @license MIT
  * @copyright Copyright (c) Yusuke Kamiyamane
@@ -70,7 +70,7 @@ export function detectMachineTranslation(): () => void {
         return;
       }
 
-      window.dispatchEvent(new CustomEvent('machineTranslationDetected'));
+      window.dispatchEvent(new CustomEvent('machinetranslationdetected'));
       observer?.disconnect();
       observer = null;
     });
